3560debe227f5caa0b178476b389503ce2117b19
jjgeek.sis <jjgeek.sis@gmail.com>
2017-08-25 20:05:32 -0400

    creacion de propuestas y listas. Integracinnn con bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,10 +18,14 @@ import { AppRoutingModule } from './app-routing.module';
     HomepageComponent,
     DocumentsComponent,
     ProposalListComponent,
+    ProposalNewComponent,
+    ProposalShowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
 export class ProposalListComponent {
-  
+  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://www.google.com', 'Ruby on Rails', 150, 128, 15, 'goo@gle.com')
+  proposalTwo: Proposal = new Proposal(150, 'Abc Corp', 'http://www.google.com', 'Ruby on Rails', 150, 128, 15, 'goo@gle.com')
+  proposalThree: Proposal = new Proposal(100, 'XYZ Company', 'http://www.google.com', 'Ruby on Rails', 150, 128, 15, 'goo@gle.com')
+  proposals: Proposal[] = [
+    this.proposalOne, 
+    this.proposalTwo, 
+    this.proposalThree
+  ]
 }
